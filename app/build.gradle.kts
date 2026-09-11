a5bce9caf738807ef7e63130b2c4ec2fb5be024f
Wikrytas <wikrytas.noliks2017@gmail.com>
2026-09-11 19:52:54 +0300

    release: v1.0.4 (code 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.wikrytas.coolplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
