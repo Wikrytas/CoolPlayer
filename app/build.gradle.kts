c2e54700524f846d96c16160b7929d80505da2da
Wikrytas <wikrytas.noliks2017@gmail.com>
2026-09-11 20:59:16 +0300

    release: v1.0.6 (code 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wikrytas.coolplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.5"
+        versionCode = 10
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
