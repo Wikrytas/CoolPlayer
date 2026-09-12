56f57518d2bbf117a56aff6f8c899d3592d959ba
Wikrytas <wikrytas.noliks2017@gmail.com>
2026-09-12 22:03:58 +0300

    release: v1.0.26 (code 29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wikrytas.coolplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.0.25"
+        versionCode = 29
+        versionName = "1.0.26"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
